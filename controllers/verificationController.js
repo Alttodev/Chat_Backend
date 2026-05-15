3e9bb307aeb1354956e410dfb0076623533f826e
Alttodev <alttodev@gmail.com>
2026-05-15 22:28:46 +0530

    update email

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -13,15 +13,17 @@ require("dotenv").config();
 //   });
 // };
 
-const createTransporter = nodemailer.createTransport({
-  host: process.env.BREVO_SMTP_HOST,
-  port: Number(process.env.BREVO_SMTP_PORT),
-  secure: false,
-  auth: {
-    user: process.env.BREVO_SMTP_USER,
-    pass: process.env.BREVO_SMTP_PASS,
-  },
-});
+const createTransporter = () => {
+  return nodemailer.createTransport({
+    host: process.env.BREVO_SMTP_HOST,
+    port: Number(process.env.BREVO_SMTP_PORT),
+    secure: false,
+    auth: {
+      user: process.env.BREVO_SMTP_USER,
+      pass: process.env.BREVO_SMTP_PASS,
+    },
+  });
+};
 
 const requestVerification = async (req, res) => {
   try {
